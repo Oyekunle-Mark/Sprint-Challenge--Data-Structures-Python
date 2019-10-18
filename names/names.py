41502bd78c3e91e0d114e31d42057b89bd30df4e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-18 09:45:51 +0100

    First pass solution at optimization

--- names/names.py
+++ names/names.py
@@ -1,4 +1,6 @@
 import time
+# import the Counter class and deque function from the collections library
+from collections import Counter, deque
 
 start_time = time.time()
 
@@ -6,15 +8,25 @@ f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# create a counter of names_1
+names_1 = Counter(names_1)
+
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
+# create a counter of names_2
+names_2 = Counter(names_2)
 
-for name_1 in names_1:
-    if name_1 in names_2:
-        duplicates.append(name_1)
+# initialize a deque to hold the duplicates
+duplicates = deque()
+
+# for every key in the names_1 counter
+for name in names_1:
+    # if the key is also present in names_2 counter
+    if name in names_2:
+        # add it to the duplicates deque
+        duplicates.append(name)
 
 end_time = time.time()
 
